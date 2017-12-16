de0cbba578e224d1a5bd5c66187075dc134d039c
jdew192837 <jdew192837@gmail.com>
2017-12-16 09:37:56 -0600

    Add carousel and logic to display icon beside clicked tag

--- main.js
+++ main.js
@@ -3,20 +3,33 @@ $(document).ready(function() {
   // Whenever an element with id is clicked, run the code
   console.log("Test");
   $(".btn-tag").click(function(eventObject) {
+    // Prevent default click
+    eventObject.preventDefault();
     console.log("Button Clicked");
     console.log(eventObject);
+
+    // Remove all icons
+    var names = [ "All", "php", "JS", "mysql", "CSS", "Java", "HTML", "MariaDB" ]
+    for (var i = 0; i < names.length; i++ ) {
+      elementID = "#" + names[i];
+      $(elementID).html(names[i]);
+    }
+
     // Get html
     var tag = $(this).html();
+    console.log(tag);
+
     // Gets all elements with class panel
     var totalCards = document.getElementsByClassName("card").length;
     console.log(totalCards);
-    // Prevent default click
-    eventObject.preventDefault();
+
     // Loop through the list
     for (var x = 1; x <= totalCards; x++  ){
+      console.log("In Loop");
       // If this element contains the tag
       if(JSON.stringify($("#librehealth-project-" + x ).html()).indexOf(tag) >= 0 ){
-        // Hide it, fade it in        
+        // Hide it, fade it in
+        console.log("Should be hiding and fading...");
         $("#librehealth-project-" + x ).hide();
         $("#librehealth-project-" + x ).fadeIn();
       } else {
@@ -24,5 +37,10 @@ $(document).ready(function() {
         $("#librehealth-project-" + x ).hide();
       };
     }
+
+    // Add icon for the clicked tag
+    var icon = '<i class="fa fa-circle icon"></i> ' + tag;
+    var elementID = "#" + tag;
+    $(elementID).html(icon);
   });
 });
